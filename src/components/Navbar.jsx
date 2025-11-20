2b6a7b4c2f5e1e6de0c6391e9cbb54b75df66123
Kevin CANAJ <kevin.canaj@lmdsio.fr>
2025-11-20 17:35:18 +0100

    partie 3 vraiment finie

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import { useAuth } from "../context/AuthContext";
 
 function Navbar(){
+    const { token, logoutUser } = useAuth();
     return(
         <nav className="navbar">
             <span className="logo">GSB Frais</span>
             <div className="links-left">
                 <Link to="/">Accueil</Link>
-                <Link to="/dashboard">Tableau de bord</Link>
+                {token && <Link to="/dashboard">Tableau de bord</Link>}
             </div>
             <div className="links-right">
-                <Link to="">Déconnexion</Link>
-                <Link to="/login">Connexion</Link>
+                {token ? (
+                  <button onClick={logoutUser} style={{background:"none",border:"none",color:"white",cursor:"pointer"}}>Déconnexion</button>
+                ) : (
+                  <Link to="/login">Connexion</Link>
+                )}
             </div>
         </nav>
     );
